17b1cb726bc12de5f220c70fe50570456c6aff77
edgar gomez <edgarjgp@gmail.com>
2015-06-20 12:27:34 +0200

    Added custom version of devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root "pages#home"
+     root "pages#home"
+
+     devise_for :users, controllers: {registrations: 'registrations'}
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
